7d255e72ca9ad05ef31aa010fd5018d2df84daa3
Neal Fultz <nfultz@gmail.com>
2020-04-27 13:51:34 -0700

    Fix return class of hdr.arcsin

---
+++ tests/test-extra-distr.R
@@ -0,0 +1,9 @@
+
+
+stopifnot(all.equal(
+  stat.extend::HDR.arcsine(.5, 0, 1), # should match beta(.5, .5)
+  structure(list(list(l = 0, r = 0.146446609406726, lc = TRUE, 
+                      rc = TRUE), list(l = 0.853553390593274, r = 1, lc = TRUE, 
+                                       rc = TRUE)), class = c("hdr", "interval"), domain = "R", method = "Computed using nlm optimisation with 0 iterations (code = 1)", probability = 0.5, distribution = "standard arcsine distribution")
+))
+
