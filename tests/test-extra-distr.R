9d586e72bb64d9bc7eb55537609cb6c0c7cbcf9f
Neal Fultz <nfultz@gmail.com>
2020-04-29 13:03:37 -0700

    invgamma distr tests

--- tests/test-extra-distr.R
+++ tests/test-extra-distr.R
@@ -7,3 +7,10 @@ stopifnot(all.equal(
                                        rc = TRUE)), class = c("hdr", "interval"), domain = "R", method = "Computed using nlm optimisation with 0 iterations (code = 1)", probability = 0.5, distribution = "standard arcsine distribution")
 ))
 
+
+
+stopifnot(all.equal(
+  stat.extend::HDR.invgamma(.5, shape=1),
+  structure(list(list(l = 0.224157477631746, r = 1.49184317616973, 
+                    lc = TRUE, rc = TRUE)), class = c("hdr", "interval"), domain = "R", method = "Computed using nlm optimisation with 11 iterations (code = 1)", probability = 0.5, distribution = "inverse gamma distribution with shape = 1 and scale = 1")
+))
